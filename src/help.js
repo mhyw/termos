ba1e964695eb7a71caff57ca388b25204f00c0cc
mhyw <74250382+mhyw@users.noreply.github.com>
2021-01-09 20:24:45 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,83 @@
+const help = (prefix) => { 
+	return ` hai omm, berikut adalah beberapa fitur RANDOM BOT.  
+┏━━°❀ ❬ *TENTANG BOT* ❭ ❀°━━━┓
+┃
+┣➥  _${prefix}info _
+┣➥  _${prefix}sewa _
+┣➥  _${prefix}donate _
+┣➥  _${prefix}owner _
+┣➥  _${prefix}ping _
+┣➥  _${prefix}bugreport [lapor bug] _
+┣➥  _${prefix}request  [minta fitur] _
+┃
+┣━━━━°❀ ❬ *MEDIA* ❭ ❀°━━━━━⊱
+┃
+┣➥  _${prefix}sticker _
+┣➥  _${prefix}tsticker _
+┣➥  _${prefix}toimg _
+┣➥  _${prefix}nulis _
+┣➥  _${prefix}ocr _
+┣➥  _${prefix}ytsearch _
+┣➥  _${prefix}ytmp3 _
+┣➥  _${prefix}tiktok _
+┣➥  _${prefix}tiktokstalk _
+┣➥  _${prefix}tts _
+┣➥  _${prefix}tes _
+┣➥  _${prefix}meme _
+┣➥  _${prefix}memeindo _
+┣➥  _${prefix}url2img _
+┣➥  _${prefix}wait _
+┃
+┣━━━━━°❀ ❬ *GRUP* ❭ ❀°━━━━━⊱
+┃
+┣➥  _${prefix}add _
+┣➥  _${prefix}kick _
+┣➥  _${prefix}promote _
+┣➥  _${prefix}demote _
+┣➥  _${prefix}welcome _
+┣➥  _${prefix}simi _
+┣➥  _${prefix}simih _
+┣➥  _${prefix}gc [buka/tutup] _
+┣➥  _${prefix}tagme _
+┣➥  _${prefix}hidetag _
+┣➥  _${prefix}tagall _
+┣➥  _${prefix}tagall2 _
+┣➥  _${prefix}tagall3 _
+┣➥  _${prefix}infogc _
+┣➥  _${prefix}linkgroup _
+┣➥  _${prefix}listadmins _
+┃
+┣━━━━━°❀ ❬ *OTHER* ❭ ❀°━━━━━⊱
+┃
+┣➥  _${prefix}blocklist _
+┣➥  _${prefix}hilih _
+┃
+┣━━━━°❀ ❬ *OWNER* ❭ ❀°━━━━━⊱
+┃
+┣➥  _${prefix}setprefix _
+┣➥  _${prefix}bc _
+┣➥  _${prefix}clearall _
+┣➥  _${prefix}clone _
+┣➥  _${prefix}leave _
+┃
+┣━━━━━°❀ ❬ *NOTE* ❭ ❀°━━━━━⊱
+┃
+┣➥ ~coret~ = fitur mati/error
+┣➥ adda yg error segera ketik 
+┃ *${prefix}bugreport (masalah)
+┣➥ request fitur ketik *${prefix}request
+┃ (fitur yg ingin di request)
+┣➥ 🛇gk boleh keras🛇
+┣➥ ☡keras☡ : coli, ejek, berantem
+┣➥ sebelum memasukkan bot ke group
+┃ harap izin owner
+┣➥ pengen jadi bot chat owner
+┣➥ owner : wa.me/6281803390353/*whatsapp lo*/
+┣➥ thx to : XP-TN, RIDHO, ST4RZ, RANDOM,
+┃MHANKBARBAR, ARUGAZ, TOBZ, HEROKU, 
+┃MT MANAGER, QUICKEDIT+, DLL
+┣━━━━━━━━━━━━━━━━━━━━
+┃ *POWERED BY RANDOMSC*
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+exports.help = help
